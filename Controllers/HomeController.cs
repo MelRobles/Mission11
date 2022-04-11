1eb761739e528bd5d15c8463d84cad0db173dd2e
MelRobles <testMelEmail@test.com>
2022-04-11 14:23:30 -0600

    full code

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission7_Books.Models;
+using Mission7_Books.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission7_Books.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private IBookProjectRepository repo;
+
+        //I'm keeping the code below for notes
+
+        //private BookstoreContext context { get; set; }
+
+        //public HomeController(BookstoreContext temp) => context => temp;
+
+        public HomeController (IBookProjectRepository temp)
+        {
+            repo = temp;
+        }
+        public IActionResult Index(string bookCategory, int pageNum = 1) //one here is the default
+        {
+            int pageSize = 5;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .Where(b => b.Category == bookCategory || bookCategory == null)
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks =
+                        (bookCategory == null
+                            ? repo.Books.Count()
+                            : repo.Books.Where(x => x.Category == bookCategory).Count()),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
+        }
+
+        //public IActionResult Index() => View();
+        //These are the same thing.I'm leaving this code for notes purposes
+    }
+}
---
+++ Models/ViewModels/BooksViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Mission7_Books.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission7_Books.Models.ViewModels
+{
+    public class BooksViewModel
+    {
+        public IQueryable<Book> Books { get; set; }
+        public PageInfo PageInfo { get; set; }
+    }
+}
